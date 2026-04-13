5e08b83f7a2fcf0551eb0684c192253fa669bc6e
Anton Kralin <kralin.dart@gmail.com>
2026-04-13 14:34:01 +0300

    add user model migrations

--- src/model/user.py
+++ src/model/user.py
@@ -7,8 +7,8 @@ from .base import BaseModel
 class User(BaseModel):
     __tablename__ = "users"
 
-    username: Mapped[str] = mapped_column(String, nullable=False, unique=True)
-    password: Mapped[str] = mapped_column(String, nullable=False)
+    username: Mapped[str] = mapped_column(String(50), nullable=False, unique=True)
+    password: Mapped[str] = mapped_column(String(200), nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean, nullable=False, default=True)
 
     def __str__(self):
