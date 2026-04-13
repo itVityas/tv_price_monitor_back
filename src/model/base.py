da9f8487e1c52b8a87d91bf6222c67cdfdd0c85f
Anton Kralin <kralin.dart@gmail.com>
2026-04-13 13:55:27 +0300

    change user model

--- src/model/base.py
+++ src/model/base.py
@@ -1,5 +1,7 @@
-from sqlalchemy import Column, Integer, DateTime, func
-from sqlalchemy.orm import DeclarativeBase
+from datetime import datetime
+
+from sqlalchemy import func
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 
 class Base(DeclarativeBase):
@@ -7,22 +9,17 @@ class Base(DeclarativeBase):
 
 
 class TimestampMixin:
-    created_at = Column(
-        DateTime(timezone=True), nullable=False, default=func.now())
-    updated_at = Column(
-        DateTime(timezone=True),
-        nullable=False,
-        default=func.now(),
-        onupdate=func.now())
+    created_at: Mapped[datetime] = mapped_column(insert_default=func.now(), nullable=False)
+    updated_at: Mapped[datetime] = mapped_column(insert_default=func.now(), nullable=False, onupdate=func.now())
 
 
 class BaseModel(Base, TimestampMixin):
     __abstract__ = True
 
-    id = Column(Integer, primary_key=True)
+    id: Mapped[int] = mapped_column(primary_key=True)
 
 
 class BaseModelOnlyId(Base):
     __abstract__ = True
 
-    id = Column(Integer, primary_key=True)
+    id: Mapped[int] = mapped_column(primary_key=True)
--- src/model/user.py
+++ src/model/user.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, String, Boolean
+from sqlalchemy import String, Boolean
+from sqlalchemy.orm import Mapped, mapped_column
 
 from .base import BaseModel
 
@@ -6,9 +7,9 @@ from .base import BaseModel
 class User(BaseModel):
     __tablename__ = "users"
 
-    username = Column(String, nullable=False, unique=True)
-    password = Column(String, nullable=False)
-    is_active = Column(Boolean, nullable=False, default=True)
+    username: Mapped[str] = mapped_column(String, nullable=False, unique=True)
+    password: Mapped[str] = mapped_column(String, nullable=False)
+    is_active: Mapped[bool] = mapped_column(Boolean, nullable=False, default=True)
 
     def __str__(self):
         return f'<user>: {self.id} {self.username}'
