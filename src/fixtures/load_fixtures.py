196fd76716c2e44fc24957a8ca48fdc9336d69d2
Anton Kralin <kralin.dart@gmail.com>
2026-05-15 14:36:38 +0300

    add currency fixture

--- src/fixtures/load_fixtures.py
+++ src/fixtures/load_fixtures.py
@@ -6,6 +6,7 @@ from sqlalchemy import select
 
 from service.security import hash_password
 from model.user import User
+from model.currency import Currency
 
 
 class FixtureLoad:
@@ -37,8 +38,29 @@ class FixtureLoad:
                 self.session.add(new_user)
                 await self.session.commit()
 
+    async def load_currencies(self, fixture_file: Path):
+        if not fixture_file.exists():
+            raise FileNotFoundError(f"Fixture file {fixture_file} not found")
+        with open(fixture_file, 'r') as file:
+            fixture_data = json.load(file)
+
+        for line in fixture_data:
+            name = line.get('name')
+
+            result = await self.session.execute(select(Currency).where(Currency.name == name))
+            existing_currency = result.scalar_one_or_none()
+
+            if not existing_currency:
+                new_currency = Currency(
+                    name=name
+                )
+                self.session.add(new_currency)
+                await self.session.commit()
+
     async def load_all_fixtures(self, fixtures_dir: Path):
         fixtures_files = fixtures_dir.glob('*.json')
         for fixture_file in fixtures_files:
             if 'user' in fixture_file.name:
                 await self.load_users(fixture_file)
+            if 'currency' in fixture_file.name:
+                await self.load_currencies(fixture_file)
